fd9ee4db5879509a533f1e04bcd9d097a4d694fb
nagabhushanam N <nagabhushanamn@gmail.com>
2020-02-28 17:58:35 +0530

    bug fix

--- T-Pay-app-v2/src/test/java/com/example/demo/pages/AccountPage.java
+++ T-Pay-app-v2/src/test/java/com/example/demo/pages/AccountPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+// 
 public class AccountPage extends BasePage {
 
 	@FindBy(xpath = "/html/body/div/pre/span[1]")
